206c6acea859900a042ca5f47d3f252e60f67994
C4lum05 <calumbowen05@gmail.com>
2026-02-09 14:43:07 +0000

    Add CSRF token to Twig global variables
    Add CSRF token as a global variable for Twig templates

--- gym.php
+++ gym.php
@@ -4,6 +4,9 @@ include("credentials/db.php");
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig = new \Twig\Environment($loader);
 
+// Make CSRF token available to all Twig templates
+$twig->addGlobal('csrf_token', csrf_token());
+
 echo $twig->render('gym.twig', [
 ]);
 ?>
