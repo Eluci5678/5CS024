f8e657cac274a1a37b39e442633fc7ec19caacfa
C4lum05 <calumbowen05@gmail.com>
2026-02-10 15:21:27 +0000

    Add CSRF token to Twig environment

--- transport.php
+++ transport.php
@@ -1,8 +1,10 @@
 <?php
+require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/vendor/autoload.php';
 include("credentials/db.php");
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig = new \Twig\Environment($loader);
+$twig->addGlobal('csrf_token', csrf_token());
 
 $stmt = $mysqli->prepare("SELECT * FROM transit_info");
 if (!$stmt) {die("Prepare failed: " . $mysqli->error);}
