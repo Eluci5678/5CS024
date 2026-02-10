7e428ee6f69e26f6d74c38fde8ba7a12417ca2d3
C4lum05 <calumbowen05@gmail.com>
2026-02-10 15:18:51 +0000

    Add CSRF token to Twig environment
    Added CSRF token as a global variable for Twig.

--- gym.php
+++ gym.php
@@ -3,10 +3,12 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/vendor/autoload.php';
 include("credentials/db.php");
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig = new \Twig\Environment($loader);
+$twig->addGlobal('csrf_token', csrf_token());
 
 $stmt = $mysqli->prepare("SELECT * FROM campus_opening_times");
 if (!$stmt) {die("Prepare failed: " . $mysqli->error);}
