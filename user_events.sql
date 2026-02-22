0d0dcdb7bc7514ab2ae717d0fd38c2384e72d35e
bcbarnes04 <bcbarnes04@icloud.com>
2026-02-22 12:20:22 +0000

    Created a table for user_events
    The creation of this table was overlooked, so as Scott requested I made it.

---
+++ user_events.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2026 at 01:18 PM
+-- Server version: 8.0.44
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db2218870`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_events`
+--
+
+CREATE TABLE `user_events` (
+  `user_id` int NOT NULL,
+  `event_id` int NOT NULL,
+  `event_join_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_events`
+--
+ALTER TABLE `user_events`
+  ADD PRIMARY KEY (`user_id`,`event_id`),
+  ADD UNIQUE KEY `event_id` (`event_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
